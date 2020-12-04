942e05e45f27cc762a869a9b0a2fdbfbc177745c
josterexcv <60027256+josterexcv@users.noreply.github.com>
2020-12-04 10:28:01 +0400

    Add files via upload

---
+++ poketMoney/poketMoney.py
@@ -0,0 +1,7 @@
+poketMoney = int(input("enter the poket money u get = "))
+if(poketMoney>500):
+    print("rich kid :)")
+elif(poketMoney>100):
+    print("average kid :|")
+else:
+    print("poor kid :(")
